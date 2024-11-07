6faf1f9b76e99f4347552faa1b0fc02e957f8315
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-07 09:23:41 -0500

    Add Post component, beginning of allowing users to post content.

---
+++ client/src/components/post.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function PostForm() {
+  return (
+    <>
+    <div>
+      <input></input>
+      <input></input>
+      <input></input>
+      </div>
+    </>
+  );
+}
+
+
+
+export default PostForm;
