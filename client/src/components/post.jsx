9dbce6b8f8a858eda22be074439b5dc48e2527f9
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-07 12:03:53 -0500

    Add post form that POSTS post data to localhost:3000/posts, not yet functional.

--- client/src/components/post.jsx
+++ client/src/components/post.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 
 function PostForm() {
+
+const handleSubmit = (e) => {
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+
+const postData = {
+title,
+message,
+};
+
+const response = fetch('http://localhost:3000/posts', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(postData),
+});
+
+console.log(response);
+
+
+}
   return (
     <>
     <div>
+      <form>
+      <input value={title} placeholder="title"></input>
+      <input value={message} placeholder="message"></input>
       <input></input>
-      <input></input>
-      <input></input>
+      <button type="submit" onClick={handleSubmit}></button>
+      </form>
       </div>
     </>
   );
