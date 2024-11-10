572b3648f9ea288e4f5fef1917fb873ef3da2723
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-09 19:27:28 -0500

    Add react router. Allows user to naigate from home page to post page.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PostForm from "./components/post";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "post",
+    element: <PostForm />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
