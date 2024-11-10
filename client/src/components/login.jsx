f8b05ba4bd52ac2a778aa48738b194a39542faa7
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-10 14:10:44 -0500

    Add login form.

---
+++ client/src/components/login.jsx
@@ -0,0 +1,16 @@
+function LogIn() {
+return (
+    <>
+    <form>
+    <input placeholder="Username or Email"></input>
+    <input placeholder="Password"></input>
+    <button type="submit">Login</button>
+    </form>
+    </>
+)
+}
+
+
+
+
+export default LogIn;
