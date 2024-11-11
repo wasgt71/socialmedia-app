64b949ed8897b2405f45033c826a33d3891c2cbd
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-11 09:59:31 -0500

    Edit login.jsx, allows user to submit login form data to server. If ok, server responds with status(200) allowing redirect to chosen route.

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,16 +1,43 @@
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 function LogIn() {
-return (
-    <>
-    <form>
-    <input placeholder="Username or Email"></input>
-    <input placeholder="Password"></input>
-    <button type="submit">Login</button>
-    </form>
-    </>
-)
+const [ username, setUsername ] = useState()
+const [ password, setPassword ] = useState();
+const navigate = useNavigate();
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const loginData = {
+    username,
+    password,
+    };
+
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
+
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+    navigate("/post");
+    }
+
 }
-
-
-
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username or Email"></input>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
 
 export default LogIn;
