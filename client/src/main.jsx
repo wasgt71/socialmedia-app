0fd6ff49d6736503f54c33671890686a8c66b570
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-12 12:31:39 -0500

    Edit main.jsx, add further routing for better navigation.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PostForm from "./components/post";
-
+import LogIn from "./components/login";
+import SignUpForm from "./components/signup";
+import DisplayPosts from "./components/displayposts";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,6 +15,18 @@ const router = createBrowserRouter([
   {
     path: "post",
     element: <PostForm />,
+  },
+  {
+    path: "login",
+    element: <LogIn />,
+  },
+  {
+    path: "signup",
+    element: <SignUpForm />,
+  },
+  {
+    path: "displayposts",
+    element: <DisplayPosts />,
   },
 ]);
 
