366de8d14668ee82c39b931cd2503ce3b3ad4ab8
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-14 12:16:00 -0500

    Edit comment.jsx, upon form submit, commentData is received by server. Server responds with req.user. Comment and User is saved to Comments state. Comments and user are mapped so users comments are displayed under posts.

--- client/src/components/comment.jsx
+++ client/src/components/comment.jsx
@@ -1,66 +1,72 @@
 import { useState } from "react";
+import myApi from "../components/axios";
 
 function Comment() {
-const [unlock, setLock] = useState(false);
-const [ showButton, setShowButton ] = useState(true);
-const [ data, setData ] = useState("");
-const [ comments, setComments ] = useState([]);
+  const [unlock, setLock] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [data, setData] = useState("");
+  const [comments, setComments] = useState([]);
 
-const handleClick = () => {
-setLock(true);
-setShowButton(false);
-console.log("unlocked");
- }
+const arr = [];
 
- const handleSubmit = (e) => {
-  e.preventDefault()
-  setLock(false);
-  setShowButton(true);
-  
-const commentData = {
-data,
+  const handleClick = () => {
+    setLock(true);
+    setShowButton(false);
+    console.log("unlocked");
+  };
 
-};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLock(false);
+    setShowButton(true);
 
-const response = fetch('http://localhost:3000/comment', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(commentData),
-});
+    const commentData = {
+      data,
+    };
 
+    const response = await myApi.post("/comment", commentData, {
+      withCredentials: true,
+    });
+    {
+      console.log(response);
+     
+      setComments((prevComments) => [
+        ...prevComments,
+        { comment: data, user: response.data.username },
+      ]);
 
-setComments((prevComments) => [...prevComments, { comment: data }]);
+      setData("");
+    }
+  };
 
-setData("");
-
-console.log(setComments);
- }
-
-
-  return(
+  return (
     <>
-    <div>
-        {showButton && (
-        <button onClick={handleClick}>Comment</button>
-        )}
+      <div>
+        {showButton && <button onClick={handleClick}>Comment</button>}
         <form onSubmit={handleSubmit}>
-        {unlock && (
-        <input value={data} onChange={(e) => setData(e.target.value)} type="text" placeholder="comment"></input>
-        )}
-        {unlock && (
-        <button type="submit">Post</button>
-        )}
+          {unlock && (
+            <input
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+              type="text"
+              placeholder="comment"
+            ></input>
+          )}
+          {unlock && <button type="submit">Post</button>}
         </form>
-        {comments.map((comment) => (
-        <p>{comment.comment}</p>
-        ))}
-        
-    </div>
+        </div>
+
+        <div>
+          {comments.map((comment) => (
+            <div>
+              <h3>{comment.user}</h3>
+              <p>{comment.comment}</p>
+            </div>
+          ))}
+          
+        </div>
     </>
-  )
-  
+  );
 }
 
 export default Comment;
