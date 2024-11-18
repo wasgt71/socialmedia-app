c048a16e1e5b73bb75aabe72037b75989c591b75
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-18 11:09:59 -0500

    Edit allUsersRouter, responds with all userinfo data.

--- server/routes/allUsersRouter.js
+++ server/routes/allUsersRouter.js
@@ -1,15 +1,19 @@
 const { Router } = require("express");
 const allUsersRouter = Router();
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 allUsersRouter.post("/", (req, res) => {
-const { userData } = req.body;
-  const allUsers = prisma.userinfo.findMany();
-
-  return res.json( allUsers );
-  
+  async function main() {
+    const allUsers = await prisma.userinfo.findMany({
+   
+    });
+    console.log(allUsers);
+    return res.json(allUsers);
+  }
+  main().then(async () => {
+    await prisma.$disconnect();
+  });
 });
 
 module.exports = allUsersRouter;
