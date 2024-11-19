9a3282959120313606cfe122e31fe61821ce6b6d
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-19 12:48:01 -0500

    Edit followRouter, recieves username from user that initial user requested to follow.

--- server/routes/followRouter.js
+++ server/routes/followRouter.js
@@ -3,8 +3,14 @@ const followRouter = Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+followRouter.post("/requests", (req, res) => {
+    const username = req.body;
+    const user = req.user;
+    console.log(user);
+    console.log({'requested to follow': username});
+    
+     return res.json(req.user);
 
-
-
+});
 
 module.exports = followRouter;
