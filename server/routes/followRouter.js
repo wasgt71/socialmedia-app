79fa611954d0e7048cb9d2e14014f9587ba45415
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-20 14:37:43 -0500

    Edit followRouter, profile.jsx sends request to accept follower at /requests/accept. followRouter removes request from requests and adds request to followers.

--- server/routes/followRouter.js
+++ server/routes/followRouter.js
@@ -5,13 +5,11 @@ const prisma = new PrismaClient();
 
 followRouter.post("/requests", async (req, res) => {
   const otherUser = req.body;
-  
-  
   const followRequest = await prisma.userinfo.update({
-   where: { username: otherUser.toString() },
+    where: { username: otherUser.toString() },
     data: {
       requests: {
-       push: "Christa",
+        push: "Christa",
       },
     },
   });
@@ -20,5 +18,32 @@ followRouter.post("/requests", async (req, res) => {
   return res.json(req.body);
 });
 
-module.exports = followRouter;
+followRouter.post("/requests/accept", async (req, res) => {
+  const user = req.user.username;
 
+
+  const confusing = await prisma.userinfo.findUnique({
+    where: { username: user },
+    
+  });
+
+  const removeRequest = confusing.requests.filter(request => request !== "Christa");
+  
+
+  const acceptRequest = await prisma.userinfo.update({
+    where: { username: user },
+    data: {
+      requests: {
+        set: removeRequest,
+      },
+      followers: {
+        push: "Christa",
+      },
+    },
+  });
+
+  console.log(acceptRequest);
+});
+
+
+module.exports = followRouter;
