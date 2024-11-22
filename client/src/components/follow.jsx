bc7c5781dac9c5ff4e08ceec873326e2c2dc5607
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-22 12:42:37 -0500

    Edit follow.jsx, Add confirmRequest. Sends all usernames to server, server verifies which of the users initial user has requested to follow. If requested, server responds with status 200. If status 200, request state is set to Requested.

--- client/src/components/follow.jsx
+++ client/src/components/follow.jsx
@@ -1,24 +1,44 @@
 import myApi from "../components/axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Follow({ username }) {
-  const [followRequest, setFollowRequest] = useState();
-  const [requestData, setRequestData] = useState([]);
+  const [lock, setLock] = useState(true);
+  const [request, setRequest] = useState("Follow");
 
   const handleFollow = async (e) => {
+    setLock(false);
     const response = await myApi.post("/follow/requests", [username], {
       withCredentials: true,
     });
     console.log(username);
   };
 
+  const confirmRequest = async () => {
+    console.log("hello");
+    const response = await myApi.post("/follow/requests/requested", [username], {
+      withCredentials: true,
+    });
+
+    if (response.status === 200) {
+      setRequest("Requested");
+      console.log(request);
+    }
+
+  };
+
+  useEffect(() => {
+    confirmRequest();
+  }, []);
+  
   return (
     <>
-      <div>
-        <button username={username} onClick={handleFollow}>
-          Follow
-        </button>
-      </div>
+      <>
+        <div>
+          <button username={username} onClick={handleFollow}>
+            {request}
+          </button>
+        </div>
+      </>
     </>
   );
 }
