288c96b0133b9872b974841a253778c0fe3e6bf2
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-24 11:37:03 -0500

    Edit follow.jsx, add confirmFollow. Sends ot herUser data to server. Verifies if req.user is a follower in other users db prisma model. If true, follow button is displayed as following.

--- client/src/components/follow.jsx
+++ client/src/components/follow.jsx
@@ -5,6 +5,9 @@ function Follow({ username }) {
   const [lock, setLock] = useState(true);
   const [request, setRequest] = useState("Follow");
 
+
+// When user clicks follow button, username of user initial user requested to follow
+// is sent to server and added to database.
   const handleFollow = async (e) => {
     setLock(false);
     const response = await myApi.post("/follow/requests", [username], {
@@ -13,21 +16,33 @@ function Follow({ username }) {
     console.log(username);
   };
 
+  // Sends usernames to server to verify if initial user has requested to follow.
   const confirmRequest = async () => {
     console.log("hello");
     const response = await myApi.post("/follow/requests/requested", [username], {
       withCredentials: true,
     });
 
+  // If initial user has requested to follow, server responds with status(200).
+  // If status(200), request state is set to "Requested". UI displays "Requested" instead of "Follow".
     if (response.status === 200) {
       setRequest("Requested");
       console.log(request);
     }
+  };
 
+  const confirmFollow = async () => {
+  const response = await myApi.post("/follow/requests/following", [username], {
+  });
+
+  if(response.status === 200) {
+   setRequest("Following");
+  }
   };
 
   useEffect(() => {
     confirmRequest();
+    confirmFollow();
   }, []);
   
   return (
