717bc8440359102c02459ee2162516ae536c61a6
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-11-28 10:54:26 -0500

    Edit otherUser.jsx, Add if(loading), try-catch blocks. Ensures better flow.

--- client/src/components/otherUser.jsx
+++ client/src/components/otherUser.jsx
@@ -1,32 +1,56 @@
 import { useState, useEffect } from "react";
 import myApi from "../components/axios";
+import Follow from "../components/follow";
 
-function OtherUser({ username }) {
-  const [value, setValue] = useState();
+function OtherUser() {
+  const [value, setValue] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const displayUser = async () => {
     try {
       const response = await myApi.post("/otherUser/display", {
         withCredentials: true,
       });
-      setValue(response.data.toString());
-      console.log(value);
+      if (response.status === 200) {
+        const username = response.data[0].username;
+        const followers = response.data[0].followers.length;
+        const following = response.data[0].following.length;
+        setValue({ username, followers, following });
+      }
     } catch (error) {
-      console.error(error);
+      console.error("Trouble fetching user data", error);
     }
+
     try {
-      const response = await myApi.post("/otherUser/remove", {
+      const response2 = await myApi.post("/otherUser/remove", {
         withCredentials: true,
       });
+
+      if (response2.status === 200) {
+        setLoading(false);
+      }
     } catch (error) {
-      console.error("Could not fetch user", error);
+      console.error("Trouble deleteing data from db", error);
     }
   };
+
+  useEffect(() => {
+    displayUser();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
-      <h1>{value}</h1>
-      <button onClick={displayUser}></button>
-      <p>hello world</p>
+      <div>
+        <h1>{value.username}</h1>
+        <h3>{value.followers}</h3>
+        <p>Followers</p>
+        <h3>{value.following}</h3>
+        <p>Following</p>
+      </div>
+      <Follow username={value.username}></Follow>
     </>
   );
 }
