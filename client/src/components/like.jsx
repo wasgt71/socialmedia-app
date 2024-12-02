db50724a83f817cc5d1f6de3f6438b10fa512ef2
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-02 13:29:05 -0500

    Edit like.jsx, Add toggleLike, likePost, removeLike. Allows user to like and unlike posts.

--- client/src/components/like.jsx
+++ client/src/components/like.jsx
@@ -1,45 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import myApi from "../components/axios";
 
-function Like() {
-  const [liked, setLike] = useState(true);
+function Like({ id }) {
   const [value, setValue] = useState("Like");
 
-
-  const handleClick = () => {
-    setLike(true);
-    if (liked === true) {
-      setValue("Liked");
-      setLike(false);
-    } else {
-      setValue("Like");
+  const toggleLike = async () => {
+    if (value === "Like") {
+      await likePost();
+    }
+    if (value === "Liked") {
+      await removeLike();
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const likeData = {
-      value,
-    };
-
-    const response = fetch("http://localhost:3000/like", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(likeData),
-    });
-
-    console.log(response);
+  const likePost = async () => {
+    setValue("Liked");
+    try {
+      const response = await myApi.post("/like", ["Liked", id], {
+        withCredentials: true,
+      });
+    } catch (error) {
+      console.error("Trouble liking post", error);
+    }
   };
 
+  const removeLike = async () => {
+    setValue("Like");
+    try {
+      const response = await myApi.post("/like/remove", ["Liked", id], {
+        withCredentials: true,
+      });
+    } catch (error) {
+      console.error("Trouble removing Like", error);
+    }
+  };
+
+  const likeStatus = async () => {
+    try {
+      const response = await myApi.post("/like/status", [id], {
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        setValue("Liked");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    likeStatus()
+  }, []);
+
+  
   return (
     <>
       <div>
-        <form onSubmit={handleSubmit}>
-        <input value={value} type="hidden"></input>
-        <button type="submit" onClick={handleClick}>{value}</button>
-        </form>
+        <button id={id} type="submit" onClick={toggleLike}>
+          {value}
+        </button>
       </div>
     </>
   );
