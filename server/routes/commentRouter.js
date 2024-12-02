851c0bb88def79cb110b9397365fa80af3a05d1c
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-02 16:34:02 -0500

    Edit commentRouter, add / and /delete requests. / creates comment & /delete deletes comment from db.

--- server/routes/commentRouter.js
+++ server/routes/commentRouter.js
@@ -1,16 +1,35 @@
 const { Router } = require("express");
 const commentRouter = Router();
-const passport = require('passport');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 
-commentRouter.post("/", (req, res) => {
-    const { data } = req.body;
-    const user = req.user;
-    console.log(req.user);
-    const comment = { data };
-     return res.json(req.user);
+commentRouter.post("/", async (req, res) => {
+    const commentData = req.body;
+    const user = req.user.username;
 
-});
+const addComment = await prisma.comment.create({
+    data: {
+        username: user,
+        text: commentData[0],
+        postId: commentData[1],
+    }
+    })
+    console.log(addComment);
+    return res.status(200).json(addComment);
+})
+     
+commentRouter.post("/delete", async (req, res) => {
+    const commentData = req.body
+    const user = req.user.username;
+
+    const deleteComment = await prisma.comment.deleteMany({
+        where: { id: Number(commentData) },
+    });
+    res.status(200).json(deleteComment);
+})
+
+
 
 module.exports = commentRouter;
 
