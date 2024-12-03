91a0b0e7dcbad88cc496dcf8489dff6a1ba10bf3
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-02 19:58:39 -0500

    Edit profile, add user posts, comments, like. Username, name, bio, followers and following.

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,21 +1,39 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import myApi from "../components/axios";
+import Comment from "../components/comment";
+import Like from "../components/like";
 
 function Profile() {
   const [user, setUser] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [lock, setLock] = useState(false);
-  
+
   const onRender = async (e) => {
-    const response = await myApi.post("/profile", {
-      withCredentials: true,
-    });
-    console.log(response);
-    setUser({
-      username: response.data.username,
-      followRequests: response.data.requests,
-    });
+    try {
+      const response = await myApi.post("/profile", {
+        withCredentials: true,
+      });
+      console.log(response);
+      setUser({
+        username: response.data.username,
+        followers: response.data.followers.length,
+        following: response.data.following.length,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      const response = await myApi.post("/profile/posts", {
+        withCredentials: true,
+      });
+      setPosts(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   useEffect(() => {
     onRender();
   }, []);
@@ -24,43 +42,42 @@ function Profile() {
     setLock(true);
   };
 
-
-  const acceptFollower = async (e) => {
-    let name = e.target.value; //Used let so upon click name data changes on render
-    console.log(name);//And is always sent to server correctly
-    const response = await myApi.post("/follow/requests/accept", [name], {
-      withCredentials: true,
-    });
-  }
-  
-   const declineFollower = async (e) => {
+  const declineFollower = async (e) => {
     let name = e.target.value;
     console.log(name);
     const response = await myApi.post("/follow/requests/decline", [name], {
       withCredentials: true,
-    })
-   }
+    });
+  };
 
   return (
     <>
       <div>
         <h1>{user.username}</h1>
+        <p>followers</p>
+        <p>{user.followers}</p>
+        <p>following</p>
+        <p>{user.following}</p>
+        <p>tristan</p>
+        <p>welcome to my instagrant page.</p>
         <img id="profile-picture" />
-        <button onClick={handleClick}>Follow Requests</button>
-
-        {lock && (
-          <>
-            {user.followRequests.map((request) => (
-              <div>
-                <h4>{request} requested to follow you.</h4>
-                <button value={request} onClick={acceptFollower}>Accept</button>
-                <button value={request} onClick={declineFollower}>Decline</button>
-              </div>
-            ))}
-          </>
-        )}
-
         <button>Edit Profile</button>
+      </div>
+      <div>
+        {posts.map((post) => {
+          return(
+            <>
+            <div>
+              <p>{post.username}</p>
+              <p>{post.caption}</p>
+              <p>Likes:</p>
+              <Like id={post.id}></Like>
+             <Comment id={post.id}></Comment>
+              <p>{post.comments}</p>
+            </div>
+            </>
+          )
+        })}
       </div>
       <div>
         <Link to="/displayposts">Home</Link>
