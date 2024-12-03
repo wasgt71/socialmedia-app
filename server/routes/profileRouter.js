fd83417c2e67ad7b370c38103c240856a0482385
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-02 19:59:44 -0500

    Edit profileRouter, add /post to fetch posts and send to client.

--- server/routes/profileRouter.js
+++ server/routes/profileRouter.js
@@ -1,20 +1,22 @@
 const { Router } = require("express");
 const profileRouter = Router();
-
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 profileRouter.post("/", (req, res) => {
-    
-     return res.json(req.user);
-
+  return res.json(req.user);
 });
 
-
-
-
-
-
-
-
-
+profileRouter.post("/posts", async (req, res) => {
+  const user = req.user.username;
+  const userPosts = await prisma.userposts.findMany({
+    where: { username: user },
+    include: {
+      comments: true,
+    },
+  });
+  console.log(userPosts);
+  res.status(200).json(userPosts);
+});
 
 module.exports = profileRouter;
