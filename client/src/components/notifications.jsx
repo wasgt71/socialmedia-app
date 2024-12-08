c0dd341f9d642721a7bfff4287521b5349411fa4
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-07 20:06:57 -0500

    added like, comment, follow and message notifications.

--- client/src/components/notifications.jsx
+++ client/src/components/notifications.jsx
@@ -7,6 +7,9 @@ import { Link } from "react-router-dom";
 function Notifications() {
   const [lock, setLock] = useState(false);
   const [likes, setLikes] = useState([]);
+  const [follow, setFollow] = useState([]);
+  const [message, setMessage] = useState([]);
+  const [comments, setComments] = useState([]);
 
   let arr = [];
 
@@ -26,27 +29,36 @@ function Notifications() {
   };
 
   const receiveComment = async () => {
-    const response = await myApi.post("/notification/comment", {
+    const response = await myApi.post("/notification/comments", {
       withCredentials: true,
     });
+    console.log(response.data);
+    setComments(response.data);
   };
 
   const receiveMessage = async () => {
     const response = await myApi.post("/notification/message", {
       withCredentials: true,
     });
+    console.log(response.data);
+    setMessage(response.data);
   };
 
   const receiveFollow = async () => {
     const response = await myApi.post("/notification/follow", {
       withCredentials: true,
     });
+    console.log(response.data);
+    setFollow(response.data.followers);
   };
 
   const unlock = () => setLock(true);
 
   useEffect(() => {
     receiveLike();
+    receiveFollow();
+    receiveMessage();
+    receiveComment();
   }, []);
 
   return (
@@ -54,15 +66,47 @@ function Notifications() {
       <div>
         <Link to="/profile">Back</Link>
         <ul>
-          {likes.map((like) => (
+          {likes.map((like) =>
             like.likes.map((user, index) => (
               <li>
-                <Link to="/otherUser">{user}</Link> 
+                <Link to="/otherUser">{user}</Link>
                 <Link to={`/viewPost?id=${like.id}`}> liked your post.</Link>
               </li>
             ))
-          ))}
+          )}
+
+          {follow.map((follower) => {
+            return (
+              <>
+                <ul>
+                  <Link to="/otherUser">{follower} started following you.</Link>
+                </ul>
+              </>
+            );
+          })}
         </ul>
+        {message.map((messages) => {
+          return (
+            <ul>
+              <Link to={`/otherUser?name=${messages.sender}`}>
+                {messages.sender} left a message in your inbox.{" "}
+                {messages.createdat}
+              </Link>
+            </ul>
+          );
+        })}
+
+        {comments.map((comment) => {
+          return (
+         comment.comments.map((com) => {
+          return (
+            <ul>
+            <Link to={`/viewPost?id=${com.postId}`}> {com.username} left a comment on your post.</Link>
+            </ul>
+          )
+         })
+        )
+        })}
       </div>
     </>
   );
